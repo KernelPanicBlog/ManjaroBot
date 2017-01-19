2a94a00ee03d95587ab695f947bc95ae7c2cc059
neoranger <neoranger@openmailbox.org>
2017-01-19 16:13:30 +0000

    modified:   functions.py

--- functions.py
+++ functions.py
@@ -49,13 +49,13 @@ def command_new_user(m):
     cid = m.chat.id
     grupo = m.chat.title
     if (m.new_chat_member.username != None and m.new_chat_member.first_name != None and m.new_chat_member.last_name != None):
-        bot.send_message(cid, 'Bienvenido' + ' ' + unicode(m.new_chat_member.first_name) + ' ' + unicode(m.new_chat_member.last_name) + '!! ' + 'A.K.A. ' + '@' + str(m.new_chat_member.username) + ' a ' + unicode(grupo) + '. ' + 'Te sugerimos leer las reglas en el mensaje anclado o click en #Normas.')
+        bot.send_message(cid, 'Bienvenido' + ' ' + unicode(m.new_chat_member.first_name) + ' ' + unicode(m.new_chat_member.last_name) + '!! ' + ' A.K.A. ' + '@' + str(m.new_chat_member.username) + ' a ' + unicode(grupo) + '. ' + 'Te sugerimos leer las reglas en el mensaje anclado o click en #Normas.')
     elif (m.new_chat_member.first_name != None and m.new_chat_member.last_name != None):
         bot.send_message(cid, 'Bienvenido' + ' ' + unicode(m.new_chat_member.first_name) + ' ' + unicode(m.new_chat_member.last_name) + '!! ' + ' A ' + grupo + '. ' + 'No tenes alias, seria mejor que te crees uno. Te sugerimos tambien leer las reglas en el mensaje anclado o click en #Normas.')
     elif (m.new_chat_member.username != None and m.new_chat_member.first_name != None):
-        bot.send_message(cid, 'Bienvenido' + ' ' + unicode(m.new_chat_member.first_name) + '!!' + 'A.K.A. ' + '@' + str(m.new_chat_member.username) + ' a ' + unicode(grupo) + '. ' + 'Te sugerimos leer las reglas en el mensaje anclado o click en #Normas.')
+        bot.send_message(cid, 'Bienvenido' + ' ' + unicode(m.new_chat_member.first_name) + '!!' + ' A.K.A. ' + '@' + str(m.new_chat_member.username) + ' a ' + unicode(grupo) + '. ' + 'Te sugerimos leer las reglas en el mensaje anclado o click en #Normas.')
     elif (m.new_chat_member.username != None and m.new_chat_member.last_name != None):
-        bot.send_message(cid, 'Bienvenido' + ' ' + unicode(m.new_chat_member.last_name) + '!!' + 'A.K.A. ' + '@' + str(m.new_chat_member.username) + ' a ' + unicode(grupo) + '. ' + 'Te sugerimos leer las reglas en el mensaje anclado o click en #Normas.')
+        bot.send_message(cid, 'Bienvenido' + ' ' + unicode(m.new_chat_member.last_name) + '!!' + ' A.K.A. ' + '@' + str(m.new_chat_member.username) + ' a ' + unicode(grupo) + '. ' + 'Te sugerimos leer las reglas en el mensaje anclado o click en #Normas.')
 
 @bot.message_handler(content_types=['left_chat_member'])
 def command_left_user(m):
