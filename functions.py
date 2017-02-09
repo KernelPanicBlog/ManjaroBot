9f966283c83adadfd00f9a5a8c872dee1dea0c84
Natanael Andrés Garrido <neoranger@openmailbox.org>
2017-02-09 14:49:45 +0000

    modified:   functions.py

--- functions.py
+++ functions.py
@@ -72,7 +72,7 @@ def command_left_user(m):
 @bot.message_handler(commands=['help'])
 def command_ayuda(m):
     cid = m.chat.id
-    bot.send_message( cid, "Comandos Disponibles:\n /blog\n /neofeed\n /manjarofeed\n /kdefeed\n /id\n /mirrors\n /keys\n /update\n /orphans\n /youtubedl\n /mpis\n /github\n /about\n /support\n /isos\n /help\n") #
+    bot.send_message( cid, "Comandos Disponibles:\n /blog\n /neofeed\n /manjarofeed\n /kdefeed\n /id\n /mirrors\n /keys\n /update\n /orphans\n /virtualbox\n /youtubedl\n /mpis\n /github\n /about\n /support\n /isos\n /help\n") #
 
 @bot.message_handler(commands=['about'])
 def command_about(m):
@@ -293,7 +293,7 @@ def command_virtualbox(m):
     markup = types.InlineKeyboardMarkup()
     itembtnvbox = types.InlineKeyboardButton('Solución', url="http://telegra.ph/Instalaci%C3%B3n-de-VirtualBox-02-09")
     markup.row(itembtnvbox)
-    bot.send_message(m.chat.id, 'Como instalar VirtualBox en Arc-Manjaro-Antergos',reply_markup=markup)
+    bot.send_message(m.chat.id, 'Como instalar VirtualBox en Arch-Manjaro-Antergos',reply_markup=markup)
 
 ###############################################################################
 #Specials functions
